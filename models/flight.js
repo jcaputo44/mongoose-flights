1756386147ce465ad201fac9e735b220e1003002
username <jcaputo66@aol.com>
2022-04-15 18:25:29 -0700

    blank table display

---
+++ models/flight.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+//optional shortcut to mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema ({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        default: 'DEN',
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        enum: []
+    },
+    departs: {
+        type: Date
+    }, 
+});
+
+
+module.exports = mongoose.model('Flight', flightSchema);
